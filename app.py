36f94bae2a6a929255d143969269fad18dd32799
202201208 <202201208@daiict.ac.in>
2023-05-27 00:25:18 +0530

    Implement some tools

--- app.py
+++ app.py
@@ -1,49 +1,45 @@
 import os
-from flask import Flask, make_response, redirect, render_template, request, send_file, after_this_request
+from flask import Flask, redirect, render_template, request, send_file, after_this_request, session
 from werkzeug.utils import secure_filename
 import cv2
 
 from image_encryption import encrypt_image, decrypt_image
-from utils import allowed_file, get_cookie
-from image_processing import apply_kernal
+from utils import allowed_file
+from image_processing import apply_kernal, negative_img, reflecting_img, resize_img, rotate_img, scale_img, shearing_img, translate_img
 
 UPLOAD_FOLDER = 'static/uploads'
 DOWNLOAD_FOLDER = 'static/downloads'
 
 
-
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
-# app.secret_key = 'secret_key'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or "$(hdKY(@H51Mgk*%^sd"
 
-
-
-
-def convertGray(filename):
-    img = cv2.imread(f"static/uploads/{filename}")
-    processedImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite(f"static/downloads/{filename}", processedImg)
-
+def get_cookie(request):
+    mode = request.cookies.get("mode")
+    if(mode == "dark"):
+        return 1
+    else:
+        return 0
+    
 @app.route("/")
 def home():
-    return render_template("index.html")
+  return render_template("index.html")
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/darkmode")
+@app.route("/togglemode")
 def darkmode():
-  resp = make_response('Setting the cookie') 
-  resp.set_cookie('mode','dark')
-  return redirect(request.referrer)
-
-@app.route("/lightmode")
-def lightmode():
-  resp = make_response('Setting the cookie') 
-  resp.set_cookie('mode','light')
-  return redirect(request.referrer)
+  dark = session.get("theme")
+  if dark == "dark":
+      session["theme"] = "light"
+  else:
+      session["theme"] = "dark"
+  url = request.referrer
+  return redirect(url)
 
 @app.route("/tools")
 def tools():
@@ -54,9 +50,20 @@ def learn():
     return redirect("/learn/introduction")
 
 @app.route("/learn/introduction")
-def intro():
-    mode = get_cookie(request)
-    return render_template("introduction.html", mode=mode)
+def learn_intro():
+    return render_template("learn/introduction.html")
+
+@app.route("/learn/encryption")
+def learn_encryption():
+    return render_template("learn/encryption.html")
+
+@app.route("/learn/embossing")
+def learn_embossing():
+    return render_template("learn/embossing.html")
+
+@app.route("/learn/enhancement")
+def learn_enhancement():
+    return render_template("learn/enhancement.html")
 
 @app.route("/tools/encryption", methods=['GET', 'POST'])
 def encryption():
@@ -102,6 +109,138 @@ def applykernel():
        return render_template("tools/kernel.html", output=False, action_path="applykernel", mat=mat)
   return render_template("tools/kernel.html", output=False, action_path="applykernel", mat=mat)
 
+@app.route("/tools/scaling", methods=['GET', 'POST'])
+def scaling():
+  if(request.method == "POST"):
+    if 'file' not in request.files:
+      return render_template("notFound.html")
+    file = request.files['file']
+    fx = float(request.form.get("fx"))
+    fy = float(request.form.get("fy"))
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      img = cv2.imread(f"static/uploads/{filename}")
+      path = f"static/downloads/{filename}"
+      scale_img(img, fx, fy, path)
+      return render_template("tools/scaling.html",output=True, original_img=filename, output_img=filename, action_path="scaling")
+    else:
+       return render_template("tools/scaling.html", output=False, action_path="scaling")
+  return render_template("tools/scaling.html", output=False, action_path="scaling")  
+
+@app.route("/tools/resizing", methods=['GET', 'POST'])
+def resizing():
+  if(request.method == "POST"):
+    if 'file' not in request.files:
+      return render_template("notFound.html")
+    file = request.files['file']
+    width = int(request.form.get("width"))
+    height = int(request.form.get("height"))
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      img = cv2.imread(f"static/uploads/{filename}")
+      path = f"static/downloads/{filename}"
+      resize_img(img, width, height, path)
+      return render_template("tools/resizing.html",output=True, original_img=filename, output_img=filename, action_path="resizing")
+    else:
+       return render_template("tools/resizing.html", output=False, action_path="resizing")
+  return render_template("tools/resizing.html", output=False, action_path="resizing")  
+
+@app.route("/tools/translate", methods=['GET', 'POST'])
+def translate():
+  if(request.method == "POST"):
+    if 'file' not in request.files:
+      return render_template("notFound.html")
+    file = request.files['file']
+    tx = int(request.form.get("tx"))
+    ty = int(request.form.get("ty"))
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      img = cv2.imread(f"static/uploads/{filename}")
+      path = f"static/downloads/{filename}"
+      translate_img(img, tx, ty, path)
+      return render_template("tools/translate.html",output=True, original_img=filename, output_img=filename, action_path="translate")
+    else:
+       return render_template("tools/translate.html", output=False, action_path="translate")
+  return render_template("tools/translate.html", output=False, action_path="translate")
+
+@app.route("/tools/rotate", methods=['GET', 'POST'])
+def rotate():
+  if(request.method == "POST"):
+    if 'file' not in request.files:
+      return render_template("notFound.html")
+    file = request.files['file']
+    x = int(request.form.get("x"))
+    y = int(request.form.get("y"))
+    deg = float(request.form.get("deg"))
+    scale = float(request.form.get("scale"))
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      img = cv2.imread(f"static/uploads/{filename}")
+      path = f"static/downloads/{filename}"
+      rotate_img(img, x, y, deg, scale, path)
+      return render_template("tools/rotate.html",output=True, original_img=filename, output_img=filename, action_path="rotate")
+    else:
+       return render_template("tools/rotate.html", output=False, action_path="rotate")
+  return render_template("tools/rotate.html", output=False, action_path="rotate")
+
+@app.route("/tools/shearing", methods=['GET', 'POST'])
+def shearing():
+  if(request.method == "POST"):
+    if 'file' not in request.files:
+      return render_template("notFound.html")
+    file = request.files['file']
+    sx = float(request.form.get("sx"))
+    sy = float(request.form.get("sy"))
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      img = cv2.imread(f"static/uploads/{filename}")
+      path = f"static/downloads/{filename}"
+      shearing_img(img, sx, sy, path)
+      return render_template("tools/shearing.html",output=True, original_img=filename, output_img=filename, action_path="shearing")
+    else:
+       return render_template("tools/shearing.html", output=False, action_path="shearing")
+  return render_template("tools/shearing.html", output=False, action_path="shearing")
+
+@app.route("/tools/reflecting", methods=['GET', 'POST'])
+def reflecting():
+  if(request.method == "POST"):
+    if 'file' not in request.files:
+      return render_template("notFound.html")
+    file = request.files['file']
+    f = int(request.form.get("flip"))
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      img = cv2.imread(f"static/uploads/{filename}")
+      path = f"static/downloads/{filename}"
+      reflecting_img(img, f, path)
+      return render_template("tools/reflecting.html",output=True, original_img=filename, output_img=filename, action_path="reflecting")
+    else:
+       return render_template("tools/reflecting.html", output=False, action_path="reflecting")
+  return render_template("tools/reflecting.html", output=False, action_path="reflecting")
+
+@app.route("/tools/negative", methods=['GET', 'POST'])
+def negative():
+  if(request.method == "POST"):
+    if 'file' not in request.files:
+      return render_template("notFound.html")
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+      filename = secure_filename(file.filename)
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+      img = cv2.imread(f"static/uploads/{filename}")
+      path = f"static/downloads/{filename}"
+      negative_img(img, path)
+      return render_template("tools/negative.html",output=True, original_img=filename, output_img=filename, action_path="negative")
+    else:
+       return render_template("tools/negative.html", output=False, action_path="negative")
+  return render_template("tools/negative.html", output=False, action_path="negative")
+
 @app.route('/downloads/<path:filename>')
 def download(filename):
     downloads = os.path.join(app.config['DOWNLOAD_FOLDER'], filename)
@@ -120,7 +259,4 @@ def download(filename):
 def not_found(e):
    return render_template("notFound.html", e=e)
 
-@app.errorhandler(500)
-def internal_error(e):
-   return render_template("notFound.html", e=e)
 app.run(debug=True, port=3000)
--- image_processing.py
+++ image_processing.py
@@ -6,3 +6,44 @@ def apply_kernal(img, mat, dest):
     final_img = cv2.filter2D(src=img, ddepth=-1, kernel=mat)
     cv2.imwrite(dest, final_img)
     
+def scale_img(img, fx, fy, path):
+  final_img = cv2.resize(img, None, fx=fx, fy=fy, interpolation=cv2.INTER_CUBIC)
+  cv2.imwrite(path, final_img)
+
+def resize_img(img, width, height, path):
+  final_img = cv2.resize(img, (width, height))
+  cv2.imwrite(path, final_img)
+
+def translate_img(img, tx, ty, path):
+  M = np.float32([[1,0,tx],[0,1,ty]])
+  dst = cv2.warpAffine(img,M,(img.shape[1],img.shape[0]))
+  cv2.imwrite(path, dst)
+
+def rotate_img(img, x, y, deg, scale, path):
+  height, width = img.shape[:2]
+  matrix = cv2.getRotationMatrix2D((x,y), deg, scale)
+  translated = cv2.warpAffine(img, matrix, (width, height))
+  cv2.imwrite(path, translated)
+
+def shearing_img(img, sx, sy, path):
+  height, width = img.shape[:2]
+  M = np.float32([[1, sx, 0], [sy, 1, 0], [0, 0, 1]])
+  sheared_img = cv2.warpPerspective(img, M, (int(width*2), int(height*2)))
+  cv2.imwrite(path, sheared_img)
+
+def reflecting_img(img, f, path):
+  final_img = cv2.flip(img, f)
+  cv2.imwrite(path, final_img)
+
+def negative_img(img, path):
+  height, width, _ = img.shape
+  for i in range(0, height - 1):
+    for j in range(0, width - 1):
+        a = img.item(i, j, 0)
+        b = img.item(i, j, 1)
+        c = img.item(i, j, 2)
+        img.itemset((i, j, 0) , 255-a)
+        img.itemset((i, j, 1) , 255-b)
+        img.itemset((i, j, 2) , 255-c)
+  cv2.imwrite(path, img)
+
